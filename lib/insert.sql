27b27018edf8e8de11f45e115329940b8b96ea91
eddy <eddy.mwaniki.27@gmail.com>
2025-03-03 15:31:42 +0300

    passes tests

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age , sex, color, temperament, alive) VALUES("Mr. Chocolate", 21, "M", "White and Black", "Sanguine" ,1);
+INSERT INTO bears(name, age , sex, color, temperament, alive) VALUES("Rowdy", 23 , "M" ,"Yellow", "Choleric",1);
+INSERT INTO bears(name, age , sex, color, temperament, alive) VALUES("Tabitha", 20 ,"F" , "Brown" , "Melancholic", 1);
+INSERT INTO bears(name, age , sex, color, temperament, alive) VALUES("Sergeant Brown", 45 , "F" , "Brown" ,"Assertive", 1);
+INSERT INTO bears(name, age , sex, color, temperament, alive) VALUES("Melissa", 19 , "F", "White", "Shy", 1);
+INSERT INTO bears(name, age , sex, color, temperament, alive) VALUES("Grinch",40, "M", "Green", "Evil", 1);
+INSERT INTO bears(name, age , sex, color, temperament, alive) VALUES("Wendy",30,"F","White and Black", "Sassy",1);
+INSERT INTO bears(name, age , sex, color, temperament, alive) VALUES(NULL, NULL, "M" ,"Brown","Aggresive",1);
